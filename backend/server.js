e5be0f28b5e357c6bb310057ba9ab297c7c23a98
Federico <federicolucaspontoriero@gmail.com>
2022-07-11 19:08:33 -0300

    added error handling

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 
+const { urlencoded } = require('express')
 const express = require('express')
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
 
 const app = express()
+
+app.use(express.json())
+app.use(urlencoded)
 
 app.get('/api/goals', (req, res) => {
   res.send('Get goals')
