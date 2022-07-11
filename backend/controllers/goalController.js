443bb295143fd52346dac199317ca375d22474b1
Federico <federicolucaspontoriero@gmail.com>
2022-07-11 19:55:19 -0300

    added model for goals

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,22 +1,31 @@
-const getGoals = (req, res) => {
-  res.status(200).json({message: 'Get goals'})
-}
+const asyncHandler = require('express-async-handler')
 
-const setGoals= (req, res) => {
-  if(!req.body.text) {
+const Goal = require('../model/goalModel')
+
+const getGoals = asyncHandler(async (req, res) => {
+  const goals = await Goal.find()
+
+  res.status(200).json(goals)
+})
+
+const setGoals = asyncHandler(async (req, res) => {
+  if (!req.body.text) {
     res.status(400)
     throw new Error('Please add a text field')
   }
-  res.status(200).json({message: 'Set goal'})
-}
+  const goal = await Goal.create({
+    text: req.body.text
+  })
+  res.status(200).json(goal)
+})
 
-const updateGoal = (req, res) => {
-  res.status(200).json({message:`Update goal ${req.params.id}`})
-}
+const updateGoal = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: `Update goal ${req.params.id}` })
+})
 
-const deleteGoal = (req, res) => {
-  res.status(200).json({message: `Delete goal ${req.params.id}`})
-}
+const deleteGoal = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: `Delete goal ${req.params.id}` })
+})
 
 module.exports = {
   getGoals,
