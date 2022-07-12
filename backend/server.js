f06da48591d33453166452583b755bc4012afa22
Federico <federicolucaspontoriero@gmail.com>
2022-07-12 14:42:52 -0300

    added colors and db config

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,22 @@
 
-const { urlencoded } = require('express')
+const colors = require('colors')
 const express = require('express')
 const dotenv = require('dotenv').config()
+
 const port = process.env.PORT || 5000
+
+const { errorHandler } = require('./middlewares/errorMiddleware')
+const connectDB = require('./config/db')
+
+connectDB()
 
 const app = express()
 
 app.use(express.json())
-app.use(urlencoded)
+app.use(express.urlencoded({ extended: false }))
 
-app.get('/api/goals', (req, res) => {
-  res.send('Get goals')
-})
+app.use('/api/goals', require('./routes/goalRoutes'))
+
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`Server started on port: ${port}`))
