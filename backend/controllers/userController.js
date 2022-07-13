bd6847e94d98a9c8001440d988d1bd9f8c6b7076
Federico <federicolucaspontoriero@gmail.com>
2022-07-13 18:59:56 -0300

    Added dependencies and registration validation

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,17 +1,28 @@
-const registerUser = (req, res) => {
-    res.json({message: 'Register User'})
-}
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const asyncHandler = require('express-async-handler');
 
-const loginUser = (req, res) => {
-    res.json({message: 'Login User'})
-}
+const User = require('../models/userModel');
 
-const getMe = (req, res) => {
-    res.json({message: 'User data display'})
-}
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body
+
+  if (!name || !email || !password) {
+    res.status(400)
+    throw new Error('Please add all fields')
+  }
+})
+
+const loginUser = asyncHandler(async (req, res) => {
+  res.json({ message: 'Login User' })
+})
+
+const getMe = asyncHadler(async (req, res) => {
+  res.json({ message: 'User data display' })
+})
 
 module.exports = {
-    registerUser,
-    loginUser,
-    getMe,
+  registerUser,
+  loginUser,
+  getMe,
 }
