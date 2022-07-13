d06b70e198b1510265a01ad174dfc92b1e0a4590
Federico <federicolucaspontoriero@gmail.com>
2022-07-13 19:09:08 -0300

    added registration logic

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -11,6 +11,32 @@ const registerUser = asyncHandler(async (req, res) => {
     res.status(400)
     throw new Error('Please add all fields')
   }
+
+  const userExists = await User.findOne({ email })
+  if (userExists) {
+    res.status(400)
+    throw new Error('User already exists')
+  }
+
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(password, salt)
+
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  })
+
+  if (user) {
+    res.status(200).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid user data')
+  }
 })
 
 const loginUser = asyncHandler(async (req, res) => {
