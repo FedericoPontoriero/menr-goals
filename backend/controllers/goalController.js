50023da171e2581fd410ddec7553fa05332d0eef
Federico <federicolucaspontoriero@gmail.com>
2022-07-14 17:01:55 -0300

    Added protection for routes

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,9 +1,10 @@
 const asyncHandler = require('express-async-handler')
 
 const Goal = require('../model/goalModel')
+const User = require('../model/userModel')
 
 const getGoals = asyncHandler(async (req, res) => {
-  const goals = await Goal.find()
+  const goals = await Goal.find({ user: req.user.id })
 
   res.status(200).json(goals)
 })
@@ -13,9 +14,12 @@ const setGoals = asyncHandler(async (req, res) => {
     res.status(400)
     throw new Error('Please add a text field')
   }
+
   const goal = await Goal.create({
-    text: req.body.text
+    text: req.body.text,
+    user: req.user.id,
   })
+
   res.status(200).json(goal)
 })
 
@@ -26,6 +30,16 @@ const updateGoal = asyncHandler(async (req, res) => {
     res.status(400)
     throw new Error('Goal not found')
   }
+  const user = await User.findById(req.user.id)
+  if (!user) {
+    res.status(401)
+    throw new Error('User not found')
+  }
+  if (goal.user.toString() !== user.id) {
+    res.status(401)
+    throw new Error('User not authorized')
+  }
+
   const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, { new: true })
 
   res.status(200).json(updatedGoal)
@@ -37,6 +51,15 @@ const deleteGoal = asyncHandler(async (req, res) => {
   if (!goal) {
     res.status(400)
     throw new Error('Goal not found')
+  }
+
+  if (!user) {
+    res.status(401)
+    throw new Error('User not found')
+  }
+  if (goal.user.toString() !== user.id) {
+    res.status(401)
+    throw new Error('User not authorized')
   }
 
   await goal.remove()
--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,14 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const { getGoals, setGoals, updateGoal, deleteGoal } = require('../controllers/goalController')
+const { protect } = require('../middlewares/authMiddleware')
 
-router.get('/', getGoals)
-
-router.post('/', setGoals)
-
-router.put('/:id', updateGoal)
-
-router.delete('/:id', deleteGoal)
-
+router.get('/', protect, getGoals)
+router.post('/', protect, setGoals)
+router.put('/:id', protect, updateGoal)
+router.delete('/:id', protect, deleteGoal)
 
 module.exports = router
