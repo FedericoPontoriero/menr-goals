12f440f53c7388ffb104b25ac83b623441e1d22b
Federico <federicolucaspontoriero@gmail.com>
2022-07-27 00:32:33 -0300

    Added service for the frontend in production

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,32 @@
+const path = require("path");
+const colors = require("colors");
+const express = require("express");
+const dotenv = require("dotenv").config();
 
-const colors = require('colors')
-const express = require('express')
-const dotenv = require('dotenv').config()
+const port = process.env.PORT || 5000;
 
-const port = process.env.PORT || 5000
+const { errorHandler } = require("./middlewares/errorMiddleware");
+const connectDB = require("./config/db");
 
-const { errorHandler } = require('./middlewares/errorMiddleware')
-const connectDB = require('./config/db')
+connectDB();
 
-connectDB()
+const app = express();
 
-const app = express()
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use("/api/goals", require("./routes/goalRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
 
-app.use('/api/goals', require('./routes/goalRoutes'))
-app.use('/api/users', require('./routes/userRoutes'))
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    )
+  );
+}
 
-app.use(errorHandler)
+app.use(errorHandler);
 
-app.listen(port, () => console.log(`Server started on port: ${port}`))
+app.listen(port, () => console.log(`Server started on port: ${port}`));
